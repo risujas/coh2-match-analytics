02b7db32fba72a74c451a78dbc432f4fa27aee41
risujas <56724011+risujas@users.noreply.github.com>
2020-12-22 00:41:22 +0200

    Add GetJsonResponse

--- Coh2Stats/Program.cs
+++ Coh2Stats/Program.cs
@@ -8,37 +8,49 @@ namespace Coh2Stats
 {
 	class Program
 	{
-        public static LeaderboardResponse GetLeaderboardById(int leaderboardId, int startRank, int numRanks)
-        {
-            LeaderboardResponse leaderboardResponse;
-
-            string url = "https://coh2-api.reliclink.com/community/leaderboard/getLeaderBoard2";
-            string urlParams = "?title=coh2&leaderboard_id=5&start=" + startRank.ToString() + "&count=" + numRanks.ToString();
+        public static string GetJsonResponse(string requestUrl, string requestParams)
+		{
+            string responseString;
 
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(url);
+            client.BaseAddress = new Uri(requestUrl);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            HttpResponseMessage response = client.GetAsync(urlParams).Result;
-            if (response.IsSuccessStatusCode)
+            HttpResponseMessage responseObject = client.GetAsync(requestParams).Result;
+            if (responseObject.IsSuccessStatusCode)
             {
-                string jsonResponse = response.Content.ReadAsStringAsync().Result;
-                leaderboardResponse = JsonConvert.DeserializeObject<LeaderboardResponse>(jsonResponse);
+                responseString = responseObject.Content.ReadAsStringAsync().Result;
 
             }
             else
             {
-                throw new Exception((int)response.StatusCode + " (" + response.ReasonPhrase + ")");
+                throw new Exception((int)responseObject.StatusCode + " (" + responseObject.ReasonPhrase + ")");
             }
 
             client.Dispose();
+
+            return responseString;
+		}
+
+        public static LeaderboardResponse GetLeaderboardById(int leaderboardId, int startRank, int numRanks)
+        {
+            string url = "https://coh2-api.reliclink.com/community/leaderboard/getLeaderBoard2";
+            string urlParams = "?title=coh2&leaderboard_id=5&start=" + startRank.ToString() + "&count=" + numRanks.ToString();
+
+            string jsonResponse = GetJsonResponse(url, urlParams);
+            LeaderboardResponse leaderboardResponse = JsonConvert.DeserializeObject<LeaderboardResponse>(jsonResponse);
 
             return leaderboardResponse;
         }
 
         static void Main(string[] args)
 		{
-            GetLeaderboardById(5, 1, 10);
+            var lb = GetLeaderboardById(5, 1, 10);
+            for (int i = 0; i < lb.statGroups.Count; i++)
+			{
+                Console.WriteLine(lb.statGroups[i].members[0].alias + " " + lb.statGroups[i].members[0].country + " " + lb.statGroups[i].members[0].profile_id);
+			}
+
             Console.ReadLine();
 		}
 	}
