db25f4fceb8bba05a1a60cbfb910cbac5f597bd3
risujas <56724011+risujas@users.noreply.github.com>
2020-12-22 01:28:57 +0200

    Add PlayerSummariesResponse.cs

--- Coh2Stats/Program.cs
+++ Coh2Stats/Program.cs
@@ -6,15 +6,9 @@ namespace Coh2Stats
 	{
         static void Main(string[] args)
 		{
-            var temp = PersonalStatResponse.GetPersonalStatBySteamId("76561198050674754");
+			var temp = PlayerSummariesResponse.GetPlayerSummariesBySteamId("76561198050674754");
 
-			for (int i = 0; i < temp.leaderboardStats.Count; i++)
-			{
-				if (temp.leaderboardStats[i].rank != -1)
-				{
-					Console.WriteLine(temp.leaderboardStats[i].leaderboard_id + " " + temp.leaderboardStats[i].rank);
-				}
-			}
+			Console.WriteLine(temp.steamResults.response.players[0].avatarfull);
 
             Console.ReadLine();
 		}
