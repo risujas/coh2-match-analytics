cdc6be202ad22e603e912340ad00a79fd19c1e02
risujas <56724011+risujas@users.noreply.github.com>
2021-01-14 08:33:48 +0200

    Move LeaderboardStat, PlayerIdentity and StatGroup to RelicAPI

--- Coh2Stats/RelicAPI.cs
+++ Coh2Stats/RelicAPI.cs
@@ -7,6 +7,59 @@ using Newtonsoft.Json;
 
 namespace Coh2Stats.RelicAPI
 {
+	public class PlayerIdentity
+	{
+		[JsonProperty("profile_id")] public int ProfileId { get; set; }
+		[JsonProperty("name")] public string Name { get; set; }
+		[JsonProperty("alias")] public string Alias { get; set; }
+		[JsonProperty("personal_statgroup_id")] public int PersonalStatGroupId { get; set; }
+		[JsonProperty("xp")] public int Xp { get; set; }
+		[JsonProperty("level")] public int Level { get; set; }
+		[JsonProperty("leaderboardregion_id")] public int LeaderboardRegionId { get; set; }
+		[JsonProperty("country")] public string Country { get; set; }
+
+		public int GetHighestRank(Database db)
+		{
+			var stat = db.GetHighestStatByStatGroup(PersonalStatGroupId);
+			if (stat == null)
+			{
+				return int.MaxValue;
+			}
+
+			return stat.Rank;
+		}
+
+		public void PrintPlayer()
+		{
+			Console.WriteLine(ProfileId + " " + Name + " " + PersonalStatGroupId + " " + Xp + " " + Level + " " + Country + " " + LeaderboardRegionId + " " + Alias);
+		}
+	}
+
+	public class StatGroup
+	{
+		[JsonProperty("id")] public int Id { get; set; }
+		[JsonProperty("name")] public string Name { get; set; }
+		[JsonProperty("type")] public int Type { get; set; }
+		[JsonProperty("members")] public List<PlayerIdentity> Members { get; set; }
+	}
+
+	public class LeaderboardStat
+	{
+		[JsonProperty("statGroup_id")] public int StatGroupId { get; set; }
+		[JsonProperty("leaderboard_id")] public int LeaderboardId { get; set; }
+		[JsonProperty("wins")] public int Wins { get; set; }
+		[JsonProperty("losses")] public int Losses { get; set; }
+		[JsonProperty("streak")] public int Streak { get; set; }
+		[JsonProperty("disputes")] public int Disputes { get; set; }
+		[JsonProperty("drops")] public int Drops { get; set; }
+		[JsonProperty("rank")] public int Rank { get; set; }
+		[JsonProperty("rankTotal")] public int RankTotal { get; set; }
+		[JsonProperty("regionRank")] public int RegionRank { get; set; }
+		[JsonProperty("regionRankTotal")] public int RegionRankTotal { get; set; }
+		[JsonProperty("rankLevel")] public int RankLevel { get; set; }
+		[JsonProperty("lastMatchDate")] public int LastMatchDate { get; set; }
+	}
+
 	class Leaderboard
 	{
 		public class Root
