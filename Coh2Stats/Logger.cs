56d3f86917ee295958ea3542b07af12fc89f51c6
risujas <56724011+risujas@users.noreply.github.com>
2021-03-01 20:11:08 +0200

    Create Logger.cs

---
+++ Coh2Stats/Logger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Coh2Stats
+{
+	static class Logger
+	{
+		private const string logFile = "log.txt";
+
+		public static void WriteLine(string text)
+		{
+			DateTime dt = DateTime.Now;
+
+			string time = dt.ToLongTimeString();
+			string message = time + " - " + text;
+
+			Console.WriteLine(message);
+			var file = File.AppendText(logFile);
+			file.WriteLine(message);
+			file.Close();
+		}
+
+		public static void WriteLine(string text, params object[] args)
+		{
+			DateTime dt = DateTime.Now;
+
+			text = string.Format(text, args);
+			string time = dt.ToLongTimeString();
+			string message = time + " - " + text;
+
+			Console.WriteLine(message);
+			var file = File.AppendText(logFile);
+			file.WriteLine(message);
+			file.Close();
+		}
+	}
+}
