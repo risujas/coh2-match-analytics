ddf71ffc8da3888f2a773bcebea64808d2bc4103
Johan <56724011+risujas@users.noreply.github.com>
2024-04-08 13:21:03 +0300

    Update to use new relic API endpoint

--- Coh2Stats_Net5/DatabaseHandler.cs
+++ Coh2Stats_Net5/DatabaseHandler.cs
@@ -2,76 +2,81 @@
 
 namespace Coh2Stats
 {
-    public static class DatabaseHandler
-    {
-        public static readonly PlayerDatabase PlayerDb = new PlayerDatabase();
-        public static readonly MatchDatabase MatchDb = new MatchDatabase();
-        public const int MaxBatchSize = 100;
+	public static class DatabaseHandler
+	{
+		public static readonly PlayerDatabase PlayerDb = new PlayerDatabase();
+		public static readonly MatchDatabase MatchDb = new MatchDatabase();
+		public const int MaxBatchSize = 100;
 
-        public static void Load()
-        {
-            PlayerDb.FindLeaderboardSizes();
+		public static void Load()
+		{
+			PlayerDb.FindLeaderboardSizes();
 
-            MatchDb.Load();
-            UserIO.WriteLine("Highest post-load match ID: " + MatchDb.GetHighestId());
-        }
+			MatchDb.Load();
+			UserIO.WriteLine("Highest post-load match ID: " + MatchDb.GetHighestId());
+		}
 
-        public static void Process()
-        {
-            var rankedPlayers = PlayerDb.FindRankedPlayers();
-            PlayerDb.FindPlayerDetails(rankedPlayers);
+		public static void Process()
+		{
+			var rankedPlayers = PlayerDb.FindRankedPlayers();
+			PlayerDb.FindPlayerDetails(rankedPlayers);
 
-            ProcessMatches(rankedPlayers);
-            UserIO.WriteLine("Highest post-process match ID: " + MatchDb.GetHighestId());
-        }
+			ProcessMatches(rankedPlayers);
+			UserIO.WriteLine("Highest post-process match ID: " + MatchDb.GetHighestId());
+		}
 
-        private static void ProcessMatches(List<RelicAPI.PlayerIdentity> rankedPlayers)
-        {
-            var playersToBeProcessed = rankedPlayers;
-            int oldMatchCount = MatchDb.MatchData.Count;
+		private static void ProcessMatches(List<RelicAPI.PlayerIdentity> rankedPlayers)
+		{
+			var playersToBeProcessed = rankedPlayers;
+			int oldMatchCount = MatchDb.MatchData.Count;
 
-            while (playersToBeProcessed.Count > 0)
-            {
-                int batchSize = MaxBatchSize;
-                if (playersToBeProcessed.Count < batchSize)
-                {
-                    batchSize = playersToBeProcessed.Count;
-                }
+			while (playersToBeProcessed.Count > 0)
+			{
+				int batchSize = MaxBatchSize;
+				if (playersToBeProcessed.Count < batchSize)
+				{
+					batchSize = playersToBeProcessed.Count;
+				}
 
-                UserIO.WriteLine("Retrieving match history for {0} players", playersToBeProcessed.Count);
+				UserIO.WriteLine("Retrieving match history for {0} players", playersToBeProcessed.Count);
 
-                var range = playersToBeProcessed.GetRange(0, batchSize);
-                playersToBeProcessed.RemoveRange(0, batchSize);
+				var range = playersToBeProcessed.GetRange(0, batchSize);
+				playersToBeProcessed.RemoveRange(0, batchSize);
 
-                List<int> profileIds = new List<int>();
-                for (int i = 0; i < range.Count; i++)
-                {
-                    var x = range[i];
-                    profileIds.Add(x.ProfileId);
-                }
+				List<int> profileIds = new List<int>();
+				for (int i = 0; i < range.Count; i++)
+				{
+					var x = range[i];
+					profileIds.Add(x.ProfileId);
+				}
 
-                var response = RelicAPI.RecentMatchHistory.RequestByProfileId(profileIds);
+				for (int profileIndex = 0; profileIndex < profileIds.Count; profileIndex++)
+				{
+					UserIO.WriteLine("[{0}/{1}]", profileIndex, profileIds.Count);
 
-                for (int i = 0; i < response.Profiles.Count; i++)
-                {
-                    var x = response.Profiles[i];
-                    PlayerDb.LogPlayer(x);
-                }
+					var response = RelicAPI.RecentMatchHistory.RequestByProfileId(profileIds[profileIndex]);
 
-                for (int i = 0; i < response.MatchHistoryStats.Count; i++)
-                {
-                    var x = response.MatchHistoryStats[i];
-                    MatchDb.LogMatch(x);
-                }
+					for (int i = 0; i < response.Profiles.Count; i++)
+					{
+						var x = response.Profiles[i];
+						PlayerDb.LogPlayer(x);
+					}
 
-                UserIO.AllowPause();
-            }
+					for (int i = 0; i < response.MatchHistoryStats.Count; i++)
+					{
+						var x = response.MatchHistoryStats[i];
+						MatchDb.LogMatch(x);
+					}
+				}
 
-            int newMatchCount = MatchDb.MatchData.Count;
-            int difference = newMatchCount - oldMatchCount;
-            UserIO.WriteLine("{0} new matches found", difference);
+				UserIO.AllowPause();
+			}
 
-            MatchDb.Write();
-        }
-    }
+			int newMatchCount = MatchDb.MatchData.Count;
+			int difference = newMatchCount - oldMatchCount;
+			UserIO.WriteLine("{0} new matches found", difference);
+
+			MatchDb.Write();
+		}
+	}
 }
--- Coh2Stats_Net5/RelicAPI/RecentMatchHistory.cs
+++ Coh2Stats_Net5/RelicAPI/RecentMatchHistory.cs
@@ -1,197 +1,195 @@
-﻿using Newtonsoft.Json;
-
-using System;
+﻿using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Coh2Stats.RelicAPI
 {
-    public class RecentMatchHistory
-    {
-        public class Matchhistoryreportresult
-        {
-            [JsonProperty("profile_id")] public int ProfileId { get; set; }
-            [JsonProperty("resulttype")] public int ResultType { get; set; }
-            [JsonProperty("teamid")] public int TeamId { get; set; }
-            [JsonProperty("race_id")] public int RaceId { get; set; }
-            [JsonProperty("Rank")] public int Rank { get; set; }
-            [JsonProperty("RankTotal")] public int RankTotal { get; set; }
-        }
+	public class RecentMatchHistory
+	{
+		public class Matchhistoryreportresult
+		{
+			[JsonProperty("profile_id")] public int ProfileId { get; set; }
+			[JsonProperty("resulttype")] public int ResultType { get; set; }
+			[JsonProperty("teamid")] public int TeamId { get; set; }
+			[JsonProperty("race_id")] public int RaceId { get; set; }
+			[JsonProperty("Rank")] public int Rank { get; set; }
+			[JsonProperty("RankTotal")] public int RankTotal { get; set; }
+		}
 
-        public class MatchHistoryStat
-        {
-            [JsonProperty("id")] public int Id { get; set; }
-            [JsonProperty("mapname")] public string MapName { get; set; }
-            [JsonProperty("maxplayers")] public int MaxPlayers { get; set; }
-            [JsonProperty("matchtype_id")] public int MatchTypeId { get; set; }
-            [JsonProperty("description")] public string Description { get; set; }
-            [JsonProperty("startgametime")] public int StartGameTime { get; set; }
-            [JsonProperty("completiontime")] public int CompletionTime { get; set; }
-            [JsonProperty("observertotal")] public int ObserverTotal { get; set; }
-            [JsonProperty("matchhistoryreportresults")] public List<Matchhistoryreportresult> MatchHistoryReportResults { get; set; }
+		public class MatchHistoryStat
+		{
+			[JsonProperty("id")] public int Id { get; set; }
+			[JsonProperty("mapname")] public string MapName { get; set; }
+			[JsonProperty("maxplayers")] public int MaxPlayers { get; set; }
+			[JsonProperty("matchtype_id")] public int MatchTypeId { get; set; }
+			[JsonProperty("description")] public string Description { get; set; }
+			[JsonProperty("startgametime")] public int StartGameTime { get; set; }
+			[JsonProperty("completiontime")] public int CompletionTime { get; set; }
+			[JsonProperty("observertotal")] public int ObserverTotal { get; set; }
+			[JsonProperty("matchhistoryreportresults")] public List<Matchhistoryreportresult> MatchHistoryReportResults { get; set; }
 
-            public bool HasAxisVictory()
-            {
-                for (int i = 0; i < MatchHistoryReportResults.Count; i++)
-                {
-                    var x = MatchHistoryReportResults[i];
-                    if (x.RaceId == (int)RaceId.German || x.RaceId == (int)RaceId.WGerman)
-                    {
-                        if (x.ResultType == 1)
-                        {
-                            return true;
-                        }
+			public bool HasAxisVictory()
+			{
+				for (int i = 0; i < MatchHistoryReportResults.Count; i++)
+				{
+					var x = MatchHistoryReportResults[i];
+					if (x.RaceId == (int)RaceId.German || x.RaceId == (int)RaceId.WGerman)
+					{
+						if (x.ResultType == 1)
+						{
+							return true;
+						}
 
-                        else
-                        {
-                            return false;
-                        }
-                    }
-                }
+						else
+						{
+							return false;
+						}
+					}
+				}
 
-                throw new Exception("Invalid factions");
-            }
+				throw new Exception("Invalid factions");
+			}
 
-            public bool HasRequiredRaces(RaceFlag raceFlags)
-            {
-                bool requireGerman = raceFlags.HasFlag(RaceFlag.German);
-                bool requireSoviet = raceFlags.HasFlag(RaceFlag.Soviet);
-                bool requireWestGerman = raceFlags.HasFlag(RaceFlag.WGerman);
-                bool requireAef = raceFlags.HasFlag(RaceFlag.AEF);
-                bool requireBritish = raceFlags.HasFlag(RaceFlag.British);
+			public bool HasRequiredRaces(RaceFlag raceFlags)
+			{
+				bool requireGerman = raceFlags.HasFlag(RaceFlag.German);
+				bool requireSoviet = raceFlags.HasFlag(RaceFlag.Soviet);
+				bool requireWestGerman = raceFlags.HasFlag(RaceFlag.WGerman);
+				bool requireAef = raceFlags.HasFlag(RaceFlag.AEF);
+				bool requireBritish = raceFlags.HasFlag(RaceFlag.British);
 
-                bool hasGerman = false;
-                bool hasSoviet = false;
-                bool hasWestGerman = false;
-                bool hasAef = false;
-                bool hasBritish = false;
+				bool hasGerman = false;
+				bool hasSoviet = false;
+				bool hasWestGerman = false;
+				bool hasAef = false;
+				bool hasBritish = false;
 
-                for (int i = 0; i < MatchHistoryReportResults.Count; i++)
-                {
-                    var x = MatchHistoryReportResults[i];
-                    if (x.RaceId == (int)RaceId.German)
-                    {
-                        hasGerman = true;
-                    }
-                    if (x.RaceId == (int)RaceId.Soviet)
-                    {
-                        hasSoviet = true;
-                    }
-                    if (x.RaceId == (int)RaceId.WGerman)
-                    {
-                        hasWestGerman = true;
-                    }
-                    if (x.RaceId == (int)RaceId.AEF)
-                    {
-                        hasAef = true;
-                    }
-                    if (x.RaceId == (int)RaceId.British)
-                    {
-                        hasBritish = true;
-                    }
-                }
+				for (int i = 0; i < MatchHistoryReportResults.Count; i++)
+				{
+					var x = MatchHistoryReportResults[i];
+					if (x.RaceId == (int)RaceId.German)
+					{
+						hasGerman = true;
+					}
+					if (x.RaceId == (int)RaceId.Soviet)
+					{
+						hasSoviet = true;
+					}
+					if (x.RaceId == (int)RaceId.WGerman)
+					{
+						hasWestGerman = true;
+					}
+					if (x.RaceId == (int)RaceId.AEF)
+					{
+						hasAef = true;
+					}
+					if (x.RaceId == (int)RaceId.British)
+					{
+						hasBritish = true;
+					}
+				}
 
-                if (requireGerman && !hasGerman)
-                {
-                    return false;
-                }
-                if (requireSoviet && !hasSoviet)
-                {
-                    return false;
-                }
-                if (requireWestGerman && !hasWestGerman)
-                {
-                    return false;
-                }
-                if (requireAef && !hasAef)
-                {
-                    return false;
-                }
-                if (requireBritish && !hasBritish)
-                {
-                    return false;
-                }
+				if (requireGerman && !hasGerman)
+				{
+					return false;
+				}
+				if (requireSoviet && !hasSoviet)
+				{
+					return false;
+				}
+				if (requireWestGerman && !hasWestGerman)
+				{
+					return false;
+				}
+				if (requireAef && !hasAef)
+				{
+					return false;
+				}
+				if (requireBritish && !hasBritish)
+				{
+					return false;
+				}
 
-                return true;
-            }
+				return true;
+			}
 
-            public bool HasAllowedRaces(RaceFlag raceFlags)
-            {
-                bool allowGerman = raceFlags.HasFlag(RaceFlag.German);
-                bool allowSoviet = raceFlags.HasFlag(RaceFlag.Soviet);
-                bool allowWestGerman = raceFlags.HasFlag(RaceFlag.WGerman);
-                bool allowAef = raceFlags.HasFlag(RaceFlag.AEF);
-                bool allowBritish = raceFlags.HasFlag(RaceFlag.British);
+			public bool HasAllowedRaces(RaceFlag raceFlags)
+			{
+				bool allowGerman = raceFlags.HasFlag(RaceFlag.German);
+				bool allowSoviet = raceFlags.HasFlag(RaceFlag.Soviet);
+				bool allowWestGerman = raceFlags.HasFlag(RaceFlag.WGerman);
+				bool allowAef = raceFlags.HasFlag(RaceFlag.AEF);
+				bool allowBritish = raceFlags.HasFlag(RaceFlag.British);
 
-                bool hasGerman = false;
-                bool hasSoviet = false;
-                bool hasWestGerman = false;
-                bool hasAef = false;
-                bool hasBritish = false;
+				bool hasGerman = false;
+				bool hasSoviet = false;
+				bool hasWestGerman = false;
+				bool hasAef = false;
+				bool hasBritish = false;
 
-                for (int i = 0; i < MatchHistoryReportResults.Count; i++)
-                {
-                    var x = MatchHistoryReportResults[i];
-                    if (x.RaceId == (int)RaceId.German)
-                    {
-                        hasGerman = true;
-                    }
-                    if (x.RaceId == (int)RaceId.Soviet)
-                    {
-                        hasSoviet = true;
-                    }
-                    if (x.RaceId == (int)RaceId.WGerman)
-                    {
-                        hasWestGerman = true;
-                    }
-                    if (x.RaceId == (int)RaceId.AEF)
-                    {
-                        hasAef = true;
-                    }
-                    if (x.RaceId == (int)RaceId.British)
-                    {
-                        hasBritish = true;
-                    }
-                }
+				for (int i = 0; i < MatchHistoryReportResults.Count; i++)
+				{
+					var x = MatchHistoryReportResults[i];
+					if (x.RaceId == (int)RaceId.German)
+					{
+						hasGerman = true;
+					}
+					if (x.RaceId == (int)RaceId.Soviet)
+					{
+						hasSoviet = true;
+					}
+					if (x.RaceId == (int)RaceId.WGerman)
+					{
+						hasWestGerman = true;
+					}
+					if (x.RaceId == (int)RaceId.AEF)
+					{
+						hasAef = true;
+					}
+					if (x.RaceId == (int)RaceId.British)
+					{
+						hasBritish = true;
+					}
+				}
 
-                if (!allowGerman && hasGerman)
-                {
-                    return false;
-                }
-                if (!allowSoviet && hasSoviet)
-                {
-                    return false;
-                }
-                if (!allowWestGerman && hasWestGerman)
-                {
-                    return false;
-                }
-                if (!allowAef && hasAef)
-                {
-                    return false;
-                }
-                if (!allowBritish && hasBritish)
-                {
-                    return false;
-                }
+				if (!allowGerman && hasGerman)
+				{
+					return false;
+				}
+				if (!allowSoviet && hasSoviet)
+				{
+					return false;
+				}
+				if (!allowWestGerman && hasWestGerman)
+				{
+					return false;
+				}
+				if (!allowAef && hasAef)
+				{
+					return false;
+				}
+				if (!allowBritish && hasBritish)
+				{
+					return false;
+				}
 
-                return true;
-            }
-        }
+				return true;
+			}
+		}
 
-        public class Root
-        {
-            [JsonProperty("result")] public WebRequestResult Result { get; set; }
-            [JsonProperty("matchHistoryStats")] public List<MatchHistoryStat> MatchHistoryStats { get; set; }
-            [JsonProperty("profiles")] public List<PlayerIdentity> Profiles { get; set; }
-        }
+		public class Root
+		{
+			[JsonProperty("result")] public WebRequestResult Result { get; set; }
+			[JsonProperty("matchHistoryStats")] public List<MatchHistoryStat> MatchHistoryStats { get; set; }
+			[JsonProperty("profiles")] public List<PlayerIdentity> Profiles { get; set; }
+		}
 
-        public static Root RequestByProfileId(List<int> profileIds)
-        {
-            string idString = string.Join(",", profileIds);
-            string requestUrl = "https://coh2-api.reliclink.com/community/leaderboard/getRecentMatchHistory";
-            string requestParams = "?title=coh2&profile_ids=[" + idString + "]";
+		public static Root RequestByProfileId(int profileId)
+		{
+			string requestUrl = "https://coh2-api.reliclink.com/community/leaderboard/getRecentMatchHistoryByProfileId";
+			string requestParams = "?title=coh2&profile_id=" + profileId;
 
-            return WebRequestHandler.GetStructuredJsonResponse<Root>(requestUrl, requestParams);
-        }
-    }
+			return WebRequestHandler.GetStructuredJsonResponse<Root>(requestUrl, requestParams);
+		}
+	}
 }
